c95738529907cbd2e1bb32398be1beaae4cdc4e7
Joshua Hess <jahess150@gmail.com>
2024-08-29 16:23:29 +0000

    Changed to material components in EditNote.js

--- src/components/EditNote.js
+++ src/components/EditNote.js
@@ -1,5 +1,12 @@
 import React, { useState } from "react";
 
+// Material UI Components
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import SaveAltIcon from "@mui/icons-material/SaveAlt";
+import CloseIcon from "@mui/icons-material/Close";
+
 export default function EditNote({ note, onFinish, onSave }) {
     const [newTitle, setNewTitle] = useState("");
     const [newContent, setNewContent] = useState("");
@@ -14,26 +21,38 @@ export default function EditNote({ note, onFinish, onSave }) {
     };
 
     return (
-        <div className='edit-note'>
-            <h2>Update Note</h2>
+        <Box
+            component='form'
+            noValidate
+            autoComplete='off'
+            className='edit-note'>
+            <h2>Edit Note</h2>
 
-            <label for='note-title'>New Title:</label>
-            <input
-                type='text'
-                id='note-title'
-                name='note-title'
+            <TextField
+                required
+                id='outlined-required'
+                label='Title'
                 value={newTitle}
                 onChange={(e) => setNewTitle(e.target.value)}
             />
 
-            <label for='note-content'>New Note Content:</label>
+            <label for='note-content'>Note Content:</label>
             <textarea
                 value={newContent}
-                onChange={(e) => setNewContent(e.target.value)}
-            ></textarea>
+                onChange={(e) => setNewContent(e.target.value)}></textarea>
 
-            <button onClick={handleSave}>Save</button>
-            <button onClick={handleCancel}>Cancel</button>
-        </div>
+            <Button
+                variant='contained'
+                startIcon={<SaveAltIcon />}
+                onClick={handleSave}>
+                Save
+            </Button>
+            <Button
+                variant='contained'
+                startIcon={<CloseIcon />}
+                onClick={handleCancel}>
+                Cancel
+            </Button>
+        </Box>
     );
 }
